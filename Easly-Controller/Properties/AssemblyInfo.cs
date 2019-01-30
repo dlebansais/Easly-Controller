679b3bb50d251be757c392b1f7691e81c0969a57
dlebansais <dlebansais@gmail.com>
2019-01-30 22:56:55 +0100

    Updated all configurations to C# 7.2

--- Easly-Controller/Properties/AssemblyInfo.cs
+++ Easly-Controller/Properties/AssemblyInfo.cs
@@ -32,6 +32,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.1983")]
-[assembly: AssemblyFileVersion("1.0.0.1948")]
+[assembly: AssemblyVersion("1.0.0.1984")]
+[assembly: AssemblyFileVersion("1.0.0.1949")]
 [assembly: NeutralResourcesLanguage("en-US")]
