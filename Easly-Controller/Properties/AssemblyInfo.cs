83c882d6c949583783dc8aa35f86957a74e72296
dlebansais <dlebansais@gmail.com>
2019-03-12 22:01:15 +0100

    Cleanup.

--- Easly-Controller/Properties/AssemblyInfo.cs
+++ Easly-Controller/Properties/AssemblyInfo.cs
@@ -32,6 +32,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.4157")]
-[assembly: AssemblyFileVersion("1.0.0.3897")]
+[assembly: AssemblyVersion("1.0.0.4164")]
+[assembly: AssemblyFileVersion("1.0.0.3903")]
 [assembly: NeutralResourcesLanguage("en-US")]
