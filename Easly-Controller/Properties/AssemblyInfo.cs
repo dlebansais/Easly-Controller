985a844ce064c5a11171b6a9ea6f091055d9607b
dlebansais <dlebansais@gmail.com>
2019-04-07 17:29:25 +0200

    Cleanup.

--- Easly-Controller/Properties/AssemblyInfo.cs
+++ Easly-Controller/Properties/AssemblyInfo.cs
@@ -32,6 +32,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.4550")]
-[assembly: AssemblyFileVersion("1.0.0.4173")]
+[assembly: AssemblyVersion("1.0.0.4551")]
+[assembly: AssemblyFileVersion("1.0.0.4174")]
 [assembly: NeutralResourcesLanguage("en-US")]
--- Easly-Draw/Properties/AssemblyInfo.cs
+++ Easly-Draw/Properties/AssemblyInfo.cs
@@ -32,6 +32,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.4531")]
-[assembly: AssemblyFileVersion("1.0.0.232")]
+[assembly: AssemblyVersion("1.0.0.4551")]
+[assembly: AssemblyFileVersion("1.0.0.233")]
 [assembly: NeutralResourcesLanguage("en-US")]
